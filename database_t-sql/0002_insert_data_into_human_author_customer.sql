c3e6f8ce1fde6a965bc03790c44cbe4980a3fe95
fan1ech00 <fan1ech000@tuta.io>
2022-02-23 13:28:35 +0200

    Update 0002_insert_data_into_human_author_customer.sql

---
+++ database_t-sql/0002_insert_data_into_human_author_customer.sql
@@ -0,0 +1,9 @@
+INSERT INTO Human (FirstName, LastName)
+VALUES 
+	('Richard', 'West'),
+	('Heather', 'Rodriquez'),
+	('Joanne', 'Rowling');
+
+INSERT INTO Human (FirstName, LastName, Patronymic)
+VALUES 
+	('Орлов', 'Тимофей', 'Алексеевич');
