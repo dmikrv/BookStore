22acd1e4d1ca279adf7bb4d719bcb3a9b32c5024
fan1ech00 <fan1ech000@tuta.io>
2022-02-23 13:42:56 +0200

    Update 0004_insert_data_into_genre.sql

---
+++ database_t-sql/0004_insert_data_into_genre.sql
@@ -0,0 +1,7 @@
+INSERT INTO Genre ([Name]) 
+VALUES 
+	('Fantasy'), ('Adventure'), ('Romance'), ('Contemporary'), ('Dystopian'),
+	('Mystery'), ('Horror'), ('Thriller'), ('Paranormal'), ('Historical fiction'),
+	('Science Fiction'), ('Children’s'), ('Memoir'), ('Cooking'), ('Art'),
+	('Self-help'), ('Development'), ('Motivational'), ('Health'), ('History'),
+	('Travel'), ('Families & Relationships'), ('Humor');
