dbb36fd0bcf081c40c2bf1d1887cffec3c24be46
fan1ech00 <fan1ech000@tuta.io>
2022-03-14 13:50:31 +0200

    add publisher/genre adding, removing, changing

--- Book Store/MainWindow.xaml.cs
+++ Book Store/MainWindow.xaml.cs
@@ -58,13 +58,11 @@ namespace Book_Store
             }
             else if (header == Properties.MainWindowStrings.TabItemPublisher)
             {
-                //db.Publishers.Load();
-                //dataGridPublisher.ItemsSource = db.Publishers.Local.ToBindingList();
+                UpdatePublishers();
             }
             else if (header == Properties.MainWindowStrings.TabItemGenre)
             {
-                //db.Genres.Load();
-                //dataGridGenre.ItemsSource = db.Genres.Local.ToBindingList();
+                UpdateGenres();
             }
 
         }
@@ -89,30 +87,96 @@ namespace Book_Store
             }
         }
 
+        private void UpdatePublishers()
+        {
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                listViewPublisher.ItemsSource = db.Publishers.ToList();
+            }
+            publisherNameText.Text = string.Empty;
+        }
+
+        private void UpdateGenres()
+        {
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                listViewGenre.ItemsSource = db.Genres.ToList();
+            }
+            genreNameText.Text = string.Empty;
+        }
+
+        // ---------------------------------------------------------------------------------------- //
+
+
+        private void listViewAuthors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            e.Handled = true;
+            if (listViewAuthors.SelectedIndex == -1)
+                return;
+
+            var author = listViewAuthors.SelectedItem as Author;
+
+            firstNameText.Text = author.FirstName;
+            lastNameText.Text = author.LastName ?? string.Empty;
+            patronymicText.Text = author.Patronymic ?? string.Empty;
+        }
+
+        private void listViewBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            e.Handled = true;
+            if (listViewBooks.SelectedIndex == -1)
+                return;
+
+            var book = listViewAuthors.SelectedItem as Book;
+        }
+
+        private void listViewPublisher_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            e.Handled = true;
+            if (listViewPublisher.SelectedIndex == -1)
+                return;
+
+            var publisher = listViewPublisher.SelectedItem as Publisher;
+
+            publisherNameText.Text = publisher.Name;
+        }
+
+        private void listViewGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            e.Handled = true;
+            if (listViewGenre.SelectedIndex == -1)
+                return;
+
+            var genre = listViewGenre.SelectedItem as Genre;
+
+            genreNameText.Text = genre.Name;
+        }
+
+
+        // ---------------------------------------------------------------------------------------- //
+
+
         private void addAuthorButton_Click(object sender, RoutedEventArgs e)
         {
             if (firstNameText.Text == string.Empty)
             {
-                MessageBox.Show(Properties.AddAuthorWindowStrings.MsgEmptyFirstName, Properties.MainWindowStrings.WindowTitle, 
+                MessageBox.Show(Properties.AddAuthorWindowStrings.MsgEmptyFirstName, Properties.MainWindowStrings.WindowTitle,
                     MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
             using (var db = new BookStoreContext(_connectionString))
             {
-                db.Authors.Add(new Author()
-                {
-                    FirstName = firstNameText.Text != string.Empty ? firstNameText.Text : null,
-                    LastName = lastNameText.Text != string.Empty ? lastNameText.Text : null,
-                    Patronymic = patronymicText.Text != string.Empty ? patronymicText.Text : null,
-                });
-
                 try
                 {
+                    db.Authors.Add(new Author()
+                    {
+                        FirstName = firstNameText.Text != string.Empty ? firstNameText.Text : null,
+                        LastName = lastNameText.Text != string.Empty ? lastNameText.Text : null,
+                        Patronymic = patronymicText.Text != string.Empty ? patronymicText.Text : null,
+                    });
+
                     db.SaveChanges();
-                    firstNameText.Text = string.Empty;
-                    lastNameText.Text = string.Empty;
-                    patronymicText.Text = string.Empty;
                     UpdateAuthors();
                 }
                 catch (Exception ex)
@@ -139,9 +203,6 @@ namespace Book_Store
                     author.Patronymic = patronymicText.Text != string.Empty ? patronymicText.Text : null;
 
                     db.SaveChanges();
-                    firstNameText.Text = string.Empty;
-                    lastNameText.Text = string.Empty;
-                    patronymicText.Text = string.Empty;
                     UpdateAuthors();
                 }
                 catch (Exception ex)
@@ -158,9 +219,6 @@ namespace Book_Store
                 {
                     db.Authors.Remove(listViewAuthors.SelectedItem as Author);
                     db.SaveChanges();
-                    firstNameText.Text = string.Empty;
-                    lastNameText.Text = string.Empty;
-                    patronymicText.Text = string.Empty;
                     UpdateAuthors();
                 }
                 catch (Exception ex)
@@ -170,27 +228,146 @@ namespace Book_Store
             }
         }
 
-        private void listViewAuthors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void addPublisherButton_Click(object sender, RoutedEventArgs e)
         {
-            e.Handled = true;
-            if (listViewAuthors.SelectedIndex == -1)
+            if (publisherNameText.Text == string.Empty)
+            {
+                MessageBox.Show("publisher name is empty!", Properties.MainWindowStrings.WindowTitle,
+                    MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
+            }
 
-            var author = listViewAuthors.SelectedItem as Author;
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    db.Publishers.Add(new Publisher()
+                    {
+                        Name = publisherNameText.Text
+                    });
 
-            firstNameText.Text = author.FirstName;
-            lastNameText.Text = author.LastName ?? string.Empty;
-            patronymicText.Text = author.Patronymic ?? string.Empty;
+                    db.SaveChanges();
+                    UpdatePublishers();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
-        private void listViewBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void changePublisherButton_Click(object sender, RoutedEventArgs e)
         {
-            e.Handled = true;
-            if (listViewBooks.SelectedIndex == -1)
+            if (publisherNameText.Text == string.Empty)
+            {
+                MessageBox.Show("publisher name is empty!", Properties.MainWindowStrings.WindowTitle,
+                    MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
+            }
 
-            var book = listViewAuthors.SelectedItem as Book;
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    var publisher = db.Publishers.First(a => a.Id == (listViewPublisher.SelectedItem as Publisher).Id);
+                    publisher.Name = publisherNameText.Text;
+
+                    db.SaveChanges();
+                    UpdatePublishers();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
+        private void deletePublisherButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    db.Publishers.Remove(listViewPublisher.SelectedItem as Publisher);
+                    db.SaveChanges();
+                    UpdatePublishers();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void addGenreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (genreNameText.Text == string.Empty)
+            {
+                MessageBox.Show("genre name is empty!", Properties.MainWindowStrings.WindowTitle,
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    db.Genres.Add(new Genre()
+                    {
+                        Name = genreNameText.Text
+                    });
+
+                    db.SaveChanges();
+                    UpdateGenres();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void changeGenreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (genreNameText.Text == string.Empty)
+            {
+                MessageBox.Show("genre name is empty!", Properties.MainWindowStrings.WindowTitle,
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    var genre = db.Genres.First(a => a.Id == (listViewGenre.SelectedItem as Genre).Id);
+                    genre.Name = genreNameText.Text;
+
+                    db.SaveChanges();
+                    UpdateGenres();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void deleteGenreButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new BookStoreContext(_connectionString))
+            {
+                try
+                {
+                    db.Genres.Remove(listViewGenre.SelectedItem as Genre);
+                    db.SaveChanges();
+                    UpdateGenres();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Properties.MainWindowStrings.WindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
     }
 }
